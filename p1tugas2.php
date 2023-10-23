1499a44ea16cb5fe20ad0f6b12eb4f5cbef315bb
nadiaoktarina <148373922+nadiaoktarina@users.noreply.github.com>
2023-10-23 23:03:02 +0700

    Add files via upload

---
+++ p1tugas2.php
@@ -0,0 +1,14 @@
+<?php
+
+$nrp = "20031010";
+$nama = "Nadia Oktarina";
+$umur = 19;
+$nilai = 99.99;
+
+echo "Nrp : ". $nrp . "</br>";
+echo "Nama : $nama</br>";
+print "Umur : ".$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("Nilai : %.3f</br>", $nilai);
+
+?>
